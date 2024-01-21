6d9954224895ff745443dd2a9fbd10e6a94fbde8
J-Spinks <jms2895@outlook.com>
2024-01-21 11:55:53 +0000

    update player to move forward

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ canvas.height = window.innerHeight
 
 context.fillStyle = 'black'
 context.fillRect(0, 0, canvas.width, canvas.height)
-// console.log(context)
+
 
 class Player {
   constructor({position, velocity}) {
     this.position = position // i.e. {x, y}
     this.velocity = velocity
+    this.rotation = 0
   }
 
   drawPlayer() {
     // check player model is centered:
+    context.save()
     // context.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false)
     // context.fillStyle = "orange"
     // context.fill()
-
+    context.beginPath()
     context.moveTo(this.position.x + 30, this.position.y)
     context.lineTo(this.position.x - 10, this.position.y - 10)
     context.lineTo(this.position.x - 10, this.position.y + 10)
@@ -27,6 +29,14 @@ class Player {
 
     context.strokeStyle = "springgreen"
     context.stroke()
+    context.restore()
+  }
+
+  update() {
+    this.drawPlayer()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
   }
 }
 
@@ -36,4 +46,62 @@ const player = new Player({
 })
 
 player.drawPlayer()
-console.log(player)
+// console.log(player)
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+  // console.log(animate)
+  player.update()
+
+  player.velocity.x = 0
+  if (keys.w.pressed) player.velocity.x = 1
+
+  if (keys.d.pressed) player.rotation += 0.01
+
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      console.log("w was pressed")
+      keys.w.pressed = true
+      break;
+    case 'KeyA':
+      keys.a.pressed = true
+      break;
+    case 'KeyD':
+      keys.d.pressed = true
+      break;
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      console.log("w was pressed")
+      keys.w.pressed = false
+      break;
+    case 'KeyA':
+      keys.a.pressed = false
+      break;
+    case 'KeyD':
+      keys.d.pressed = false
+      break;
+  }
+})
